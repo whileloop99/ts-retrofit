c502cba71955bd5d4fbda11255b5e8587db94de4
whileloop99 <whileloop99@gmail.com>
2020-10-15 21:57:00 +0900

    Allow empty query

--- src/baseService.ts
+++ src/baseService.ts
@@ -170,14 +170,14 @@ export class BaseService {
     const query = meta[methodName].query || {};
     const queryParams = meta[methodName].queryParams;
     for (const pos in queryParams) {
-      if (queryParams[pos]) {
+      if (queryParams[pos] !=== undefined) {
         query[queryParams[pos]] = args[pos];
       }
     }
     const queryMapIndex = meta[methodName].queryMapIndex;
     if (queryMapIndex >= 0) {
       for (const key in args[queryMapIndex]) {
-        if (args[queryMapIndex][key]) {
+        if (args[queryMapIndex][key] !=== undefined) {
           query[key] = args[queryMapIndex][key];
         }
       }
